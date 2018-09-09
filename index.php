ed4008c79a4de7da8194d21a252e854e01e1732a
ned.aoudjet <33225073+naoud@users.noreply.github.com>
2018-09-09 13:43:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP</title>
  </head>
  <body>
- <?php echo '<p>Hello World - version test- 19992 - push 7000</p>'; ?> 
+ <?php echo '<p>Hello World - version test- 1</p>'; ?> 
  </body>
 </html>
