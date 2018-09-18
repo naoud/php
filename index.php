8eff7ac57866400b01b4442a8ec9b79fa4e96b9c
ned.aoudjet <33225073+naoud@users.noreply.github.com>
2018-09-18 14:30:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP</title>
  </head>
  <body>
- <?php echo '<p>Hello World - version test-220566 of 5</p>';?>
+ <?php echo '<p>Hello World - version test-220566 of 6</p>';?>
  </body>
 </html>
